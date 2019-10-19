e132040313d9432f7572dc659f17b8790f3a649d
RavenSF <zzravenwww@gmail.com>
2019-10-19 23:40:35 +0800

    20191019regular test success

--- NovelCrawler/re/douluo4.py
+++ NovelCrawler/re/douluo4.py
@@ -11,12 +11,17 @@ with open('title_post.txt','w',encoding='utf-8') as txt1:
 real_url = []
 # '/html/body/div[10]/div[1]/div[2]/ul/li[1]'
 '/html/body/div[@class=Layout no_h]/div[@class=Con jj_pl]/div[@class=list_box]/ul/li/'
-ch_url = re.findall('a href="(.*?)">',str(title_code),re.S)
-print(len(ch_url))      #这里是每章的url
+ch_url = re.findall('<a href="(.*?)">第',str(title_code),re.S)
+ch_url.pop(0)
+ch_url.pop(0)
+ch_url.pop(0)
+print(ch_url)      #这里是每章的url
 # ch_name = re.findall('.html">(.*?)</a></dd><dd><a',str(title_code),re.S)
 #print(ch_name)     这里是每章的名字
 for url_test in ch_url:
-    real_url.append('https://www.kanunu8.com/book2/10943/' + str(url_test))  #每章网址
+    real_url.append('http://www.yunxs.com/canglanjue/' + str(url_test))  #每章网址
+
+
 
 
 
