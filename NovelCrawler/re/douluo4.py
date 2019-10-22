97cae6b65eda2bb6f42484d9cb5bd78e02c58378
RavenSF <zzravenwww@gmail.com>
2019-10-22 17:24:31 +0800

    20191022整理代码，修复第一章读取失败的问题

--- NovelCrawler/re/douluo4.py
+++ NovelCrawler/re/douluo4.py
@@ -2,41 +2,13 @@ import os
 import re
 import time
 import requests
-# from multiprocess.dummy import Pool
 
-# title_code = requests.post('http://www.yunxs.com/canglanjue/').content.decode()
-
-# #   先清空目标txt文件防止被重复写入
-# with open('终极斗罗.txt','w',encoding='utf-8') as temp:  #
-#     temp.truncate()
-
-# real_url = []
-# ch_url = re.findall('<a href="(.*?)">第',str(title_code),re.S)
-# ch_url.pop(0)
-# ch_url.pop(0)
-# ch_url.pop(0)
-# print(ch_url)      #这里是每章的url
-# for url_test in ch_url:
-#     real_url.append('http://www.yunxs.com/canglanjue/' + str(url_test))  #每章网址
-
-# num = 0
-# while (num <= len(real_url)):
-#     print("request start")
-#     code_html = requests.get(real_url[num])
-#     code_bytes = code_html.content
-#     print("decode start")
-#     code_str = code_bytes.decode("UTF-8","ignore")
-#     ar = re.findall('&nbsp;&nbsp;&nbsp;&nbsp;(.*?)<br />',code_str,re.S)
-#     with open('终极斗罗.txt','a',encoding='utf-8') as txt2:  
-#         txt2.writelines(ar) 
-#     print('第%d章打印完成'%num)
-#     num = num + 1
 
 class re_crawler(object):
 
     bookname = '终极斗罗'
     list_url = 'http://www.yunxs.com/canglanjue/'
-    list_re = '<a href="(.*?)">第'
+    list_re = '<li><a href="(.*?)">第'
     real_url = []
 
     def __init__(self):
@@ -51,7 +23,7 @@ class re_crawler(object):
         self.title_code = requests.post(self.list_url).content.decode()
         self.ch_url = re.findall(self.list_re,str(self.title_code),re.S)
         self.ch_url.pop(0)
-        self.ch_url.pop(0)
+        # self.ch_url.pop(0)
         for url_test in self.ch_url:
             self.real_url.append('http://www.yunxs.com/canglanjue/' + str(url_test))  #每章网址
 
@@ -72,9 +44,16 @@ class re_crawler(object):
         self.clearDoc()
         self.get_url_list()
         self.get_article()
+        # print(self.ch_url[0])
 
 
 
 if __name__ == '__main__':
     re_user = re_crawler()
+    time_start = time.time()
     re_user.user_call()
+    time_used = time.time() - time_start
+    print('本次爬取共用时%ds'%time_used,'，共%d章'%len(re_user.real_url))
+    # 本次爬取共用时279s ，共635章
+    av_time = time_used/(len(re_user.real_url))
+    print('平均每章用时%d'%av_time)
