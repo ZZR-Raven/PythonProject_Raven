14510a273aed7da15ef4cc5e3ec9d168719e063a
RavenSF <zzravenwww@gmail.com>
2019-10-25 23:42:01 +0800

    20191025try in dell

--- NovelCrawler/xpath/test.py
+++ NovelCrawler/xpath/test.py
@@ -1,17 +1,18 @@
-# from tqdm import tqdm
+from tqdm import tqdm
 import time
 
-# with tqdm(total=100) as pbar:
-#     for i in (range(100)):
-#         # tqdm.write("wtf")
-#         time.sleep(1)
-#         pbar.update(1)
+with tqdm(total=100) as pbar:
+    for i in (range(100)):
+        # tqdm.write("wtf")
+        time.sleep(1)
+        pbar.update(1)
 
 # tqdm.close()
-for i in range(100):
-    print('\r',end='')
-    for j in range(i):
-        print("*",end='',sep='')
-    time.sleep(0.01)
+# for i in range(100):
+#     print('\r',end='')
+#     for j in range(i):
+#         print("*",end='',sep='')
+#     time.sleep(0.01)
 
+#去其他设备试试
         
