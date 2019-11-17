e20717a6fbb063037135285f13df50f9745b0850
RavenSF <zzravenwww@gmail.com>
2019-11-17 23:36:24 +0800

    20191117删除测试代码

--- SmsCrawler/sms.py
+++ SmsCrawler/sms.py
@@ -21,15 +21,6 @@ id_list = re.findall('evaluationId=(.*?)&amp;classYearId=13',id_source,re.S)
 print(id_list)
 # id_list = ['4461', '2911', '10233', '10245', '10246', '10249', '10254', '10257', '7490', '10283', '10285', '10298', '10300', '10303', '10305', '10307', '10334', '10337', '10343', '10348', '10362', '10369', '949', '10379', '10392', '6552', '10400', '10407', '4841', '1319', '10431', '3422', '10450', '1024', '6577', '3375', '10475', '10478', '10723', '6563', '4850', '10893', '10900']
 
-# testurl = 
-# '''
-# http://xsgl.7i5q.cas.scut.edu.cn/sms2/student/module/evaluation/studentIntellectualDetail.jsp?evaluationId=4461&classYearId=13
-# '''
-# test_code = driver.get('http://xsgl.7i5q.cas.scut.edu.cn/sms2/student/module/evaluation/studentIntellectualDetail.jsp?evaluationId=4461&classYearId=13')#.content.decode(encoding = 'utf-8',errors = 'ignore')
-# test_source = driver.page_source
-# print(test_source)
-
-
 url1 = 'http://xsgl.7i5q.cas.scut.edu.cn/sms2/student/module/evaluation/studentIntellectualDetail.jsp?evaluationId='
 url2 = '&classYearId=13'
 for id in id_list:
@@ -39,21 +30,4 @@ for id in id_list:
     with open(id+'.txt','w',encoding='utf-8',errors = 'ignore') as info :
         info.write(info_source)
 
-
-
-# real_url = []
-# # with open('zhiyu.html',encoding='utf-8') as handcv:
-# #     hhh = handcv.read()
-# url_list = re.findall('<td><a href="(.*?)" target=',str(hhh))
-# for url in url_list:
-#     real_url.append('http://xsgl.7i5q.cas.scut.edu.cn' + url)  
-# scorelist = re.findall('<td>(\\d.\\d)</td>',str(hhh))
-# print(scorelist)
-# finallist = []
-# i = 0
-# for score in scorelist:
-#     if i%2 == 0:
-#         finallist.append(scorelist[i]) 
-#     i = i + 1
-# print(finallist)
 
