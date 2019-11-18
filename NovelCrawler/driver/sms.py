6046a3326e3111bd93b691da88f0e3aad82e146c
RavenSF <zzravenwww@gmail.com>
2019-11-18 10:01:24 +0800

    20191118只爬取源码，保存运行状态

--- NovelCrawler/driver/sms.py
+++ NovelCrawler/driver/sms.py
@@ -7,6 +7,7 @@ from scrapy.selector import Selector
 import scrapy
 import lxml
 from tqdm import tqdm,trange
+import time
 
 
 class driver_crawler(object):
@@ -40,6 +41,7 @@ if __name__ == '__main__':
     id_list = []
     id_list = re.findall(id_re,id_source,re.S)
     idnum = 0
+    time_start = time.time()
     try:
         with trange(len(id_list),ncols=75) as t:    
             for idnum in t:
@@ -50,6 +52,10 @@ if __name__ == '__main__':
         t.close()
         raise
     t.close()
+    time_used = time.time() - time_start
+    print('本次爬取共用时%ds'%time_used,'，共%d人'%(len(id_list)))
+    av_time = time_used/(len(id_list))
+    print('平均每人用时%fs'%av_time)
 
         # with open(id+'.txt','w',encoding='utf-8',errors = 'ignore') as info :
         #     info.write(info_source)
